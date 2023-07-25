2c47ed80e8a7b138b0f685e6107446b0007d5b51
shirolin <shirolin.com@gmail.com>
2023-07-25 17:55:08 +0800

    🐳 chore(构建脚本): 修改主题安装脚本

--- shell/install_theme_butterfly.sh
+++ shell/install_theme_butterfly.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-git clone -b master https://github.com/jerryc127/hexo-theme-butterfly.git themes/butterfly
+# git clone -b master https://github.com/jerryc127/hexo-theme-butterfly.git themes/butterfly
+
+git submodule add -b master https://github.com/jerryc127/hexo-theme-butterfly.git themes/butterfly
+
+# 更新子模块到最新版本
+# git submodule update
