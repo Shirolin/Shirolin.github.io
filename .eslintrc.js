54381acf696a99ba139704ced19a87d84d3e860e
Shirolin <shirolin.com@gmail.com>
2026-04-10 11:53:15 +0800

    chore(env): 引入 ESLint、Prettier 和 TypeScript 并集成至构建流程

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
